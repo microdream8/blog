5d5497c8afc7d27b1955b1649f4edef3aca128bb
wecode <gexiaolei@codeus.com.cn>
2019-11-18 20:00:06 +0800

    deploy

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -20,6 +20,7 @@ module.exports = {
           { text: '深入webpack', link: '/share/webpack' },
           { text: 'webpack随手记', link: '/share/webpack-note' },
           { text: '深入scratch', link: '/scratch/links' },
+          { text: '公众号开发总结', link: '/share/wechat' },
         ]
       },
       {
