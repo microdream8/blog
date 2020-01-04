e1e70cb212fc54753acb7ec2fdeffc24e94569e1
wecode <gexiaolei@codeus.com.cn>
2020-01-04 16:54:22 +0800

    deploy

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -44,6 +44,7 @@ module.exports = {
           { text: '图片相关总结', link: '/javascript/img' },
           { text: '工具函数总结', link: '/javascript/tool-fun' },
           { text: '手写代码', link: '/javascript/write-code' },
+          { text: 'js继承', link: '/javascript/inherit' },
         ]
       },
       {
