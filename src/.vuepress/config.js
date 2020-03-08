08dfa035ebe177d6901e04fdd6af038e5e76688f
wecode <gexiaolei@codeus.com.cn>
2020-03-08 20:14:00 +0800

    deploy

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -15,14 +15,8 @@ module.exports = {
         text: '教程',
         items: [
           { text: '环境配置', link: '/share/env'},
-          { text: 'eslint规则', link: '/share/eslint'},
+          // { text: 'eslint规则', link: '/share/eslint'},
           // { text: '性能优化', link: '/share/opt'},
-          { text: '深入webpack', link: '/share/webpack' },
-          { text: 'webpack随手记', link: '/share/webpack-note' },
-          { text: 'webpack配置vue', link: '/share/webpack-vue' },
-          { text: 'webpack入门', link: '/share/webpack-base' },
-          { text: '深入浅出前端脚手架', link: '/share/scaffold' },
-          { text: 'markdown语法', link: '/share/md' },
         ]
       },
       {
@@ -36,6 +30,12 @@ module.exports = {
         text: '技术积累',
         items: [
           { text: 'webpack学习', link: '/technology/webpack'},
+          { text: '深入webpack', link: '/share/webpack' },
+          { text: 'webpack随手记', link: '/share/webpack-note' },
+          { text: 'webpack配置vue', link: '/share/webpack-vue' },
+          { text: 'webpack入门', link: '/share/webpack-base' },
+          { text: '深入浅出前端脚手架', link: '/share/scaffold' },
+          { text: 'markdown语法', link: '/share/md' },
         ]
       },
       {
